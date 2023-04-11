b70174873a69146104a4dfd696c7d38189b7201f
fleiviobr <flaviogribr@gmail.com>
2023-04-11 10:50:49 -0300

    11/04

---
+++ 3108.php
@@ -0,0 +1,46 @@
+<?php
+
+$n = readline();
+
+while (0 < $n) {
+
+    $space = 0;
+    $x = readline();
+    $y = "";
+    for ($i = 0; $i < strlen($x); $i++) {
+        if ($x[$i] === " "){
+            $space++;
+        }
+    }
+
+    $xe = explode(" ", $x);
+    for($i = 0, $j = $space; $i <= $space; $i++, $j--) {
+        $y .= $xe[$j] . " ";
+    }
+    
+    for($i = 0; $i < strlen($y); $i++) {
+        if($y[$i] === "P") {
+            $y[$i] = "B";
+        }
+        if($y[$i] === "F") {
+            $y[$i] = "V";
+        }
+        if($y[$i] === "T") {
+            $y[$i] = "D";
+        }
+        if($y[$i] === "R") {
+            $y[$i] = "L";
+        }
+        if($y[$i] === "J") {
+            $y[$i] = "Z";
+        }
+        if($y[$i] === "X") {
+            $y[$i] = "S";
+        }
+    }
+
+    echo $y . PHP_EOL;
+    $n--;
+}
+
+?>
